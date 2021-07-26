650a1c4191b030bfaf3e4a7d68626abc575a6484
NoExitCondition <xschoolstuff@gmail.com>
2021-07-26 00:07:07 -0700

    Added DB connection

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 /*Import project dependencies*/
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const routes = require('./routes/api.js');
+const path = require('path');
 require('dotenv').config();
 
 /*Initalize Express server*/
 const app = express();
-
 const port = process.env.PORT || 5000;
 
 /*Handling Cors for app*/
@@ -19,8 +22,33 @@ app.use((req, res, next) => {
     res.send('Middleware works');
 })
 
+/*BodyParser*/
+app.use(bodyParser.json());
+
+/*Routing*/
+app.use('/api', routes);
+
+/*middleware Error Checking?*/
+app.use((err, req, res, next) => {
+    console.log(err);
+    next();
+});
+
 /*Listening for port*/
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
+/*DB Connection*/
+const uri = "mongodb+srv://Admin:Password@todocluster.jihcn.mongodb.next/ToDoClusters?retryWrites=true&w=majority";
+
+try{
+    mongoose.connect(
+        uri, 
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        () => console.log("MongoDB connected successfully"),
+    );       
+}
+catch(e){
+    console.log("Error connecting");
+}
