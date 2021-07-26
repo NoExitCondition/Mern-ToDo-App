1c098d54213e1a5785e76f37ac97e171439156c0
NoExitCondition <xschoolstuff@gmail.com>
2021-07-25 23:30:04 -0700

    updated routes with model

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,36 @@
 /*Dependencies*/
 const express = require('express');
 const router = express.Router();
+const Todo = require('../models/todo');
 
-/*View all tasks */
+/*View all tasks 
+  Will return all data and expose only id and action field to client
+*/
 router.get('/todos', (req, res, next) => {
-
+    Todo.find({}, 'action')
+        .then(data => res.json(data))
+        .catch(next)
 });
 
 /*Create a task*/
 router.post('/todos', (req, res, next) =>{
-
+    if(req.body.action){
+        Todo.create(req.body)
+            .then(data => res.json(data))
+            .catch(next)
+    }
+    else{
+        res.json({
+            error: "The input field is empty"
+        })
+    }
 });
 
 /*Delete a task */
 router.delete('/todos', (req, res, next) =>{
-
+    Todo.findOneAndDelete({"_id": req.params.id})
+        .then(data => res.json(data))
+        .catch(next)
 });
 
 module.exports = router;
