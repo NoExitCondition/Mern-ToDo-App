bb6f73a4ef458e93455aca01503933e068f934ed
NoExitCondition <xschoolstuff@gmail.com>
2021-07-25 23:08:35 -0700

    model + route setup

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 /*Import project dependencies*/
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+require('dotenv').config();
 
 /*Initalize Express server*/
 const app = express();
 
-/*Call body-parser and cors middleware*/
-let corsOption = {
-    origin: 'http://localhost:3000' //4000
-}
+const port = process.env.PORT || 5000;
 
-app.use(cors(bodyOptions));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-/*Create default route*/
-app.get('/', (req, res) => {
-    res.json({message: "Welcome to the REST API"});
+/*Handling Cors for app*/
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-/*Set PORT and listen for request*/
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`);;
+/*Middleware Test*/
+app.use((req, res, next) => {
+    res.send('Middleware works');
+})
+
+/*Listening for port*/
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
+
---
+++ routes/api.js
@@ -0,0 +1,20 @@
+/*Dependencies*/
+const express = require('express');
+const router = express.Router();
+
+/*View all tasks */
+router.get('/todos', (req, res, next) => {
+
+});
+
+/*Create a task*/
+router.post('/todos', (req, res, next) =>{
+
+});
+
+/*Delete a task */
+router.delete('/todos', (req, res, next) =>{
+
+});
+
+module.exports = router;
